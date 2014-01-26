996c351c3dba7688472a9ff59bf5a41ba55e06ff
Lou Leal <lewis.leal@gmail.com>
2014-01-26 11:04:07 -0500

    Add JSON response

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Acme\DemoBundle\AcmeDemoBundle()
+            new Acme\DemoBundle\AcmeDemoBundle(),
+            new Lou\TestBundle\LouTestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Lou/TestBundle/LouTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lou\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LouTestBundle extends Bundle
+{
+}
